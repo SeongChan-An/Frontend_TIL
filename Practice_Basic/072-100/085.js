fd8e65402876745fe26d96bbd6aa0a9e8b4fefb9
tjscks0510 <tjscks0510@gmail.com>
2021-12-10 16:53:22 +0900

    Test commit, using the git add -u commands

--- Practice_Basic/072-100/085.js
+++
@@ -1,4 +0,0 @@
-const lands = document.getElementsByClassName('land');
-[...lands].forEach(
-  land => land.style.fill = `rgb(${Math.floor(Math.random()*256)}, ${Math.floor(Math.random()*256)}, ${Math.floor(Math.random()*256)})`
-);
--- Practice_Basic/README.md
+++
@@ -1,2 +0,0 @@
-# Frontend_School_HTML_TIL
-Git repository for Frontend School HTML TIL from Like Lion
